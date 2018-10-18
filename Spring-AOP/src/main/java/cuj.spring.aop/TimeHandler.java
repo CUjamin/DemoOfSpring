09fdb8da584141c98e0fe5a1bfa468c162b4699d
cujamin <chbnjamin@gmail.com>
2018-10-18 22:59:26 +0800

    AOP学习

--- Spring-AOP/src/main/java/cuj.spring.aop/TimeHandler.java
+++ Spring-AOP/src/main/java/cuj.spring.aop/TimeHandler.java
@@ -1,11 +1,16 @@
 package cuj.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
 /**
  * @Auther: cujamin
  * @Date: 2018/10/18 18:42
  * @Description:
  */
+@Aspect
 public class TimeHandler {
+    @Pointcut("execution(* cuj.spring.aop.HelloWorld.doPrint(..))")
     public void printTime()
     {
         System.out.println(" 切点 CurrentTime = " + System.currentTimeMillis());
